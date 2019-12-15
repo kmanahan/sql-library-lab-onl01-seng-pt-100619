cd89c545c656233464a62e3d24db59d9f4837da0
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-12-15 20:10:09 +0000

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE characters (name TEXT, motto TEXT, )
+
+CREATE TABLE books (title TEXT, year INTEGER, series_id INTEGER)
+
+CREATE TABLE authors (name TEXT)
+
+CREATE TABLE sub_genre (name TEXT)
+
+CREATE TABLE series (title TEXT, author_id INTEGER, sub_genre_id INTEGER);
